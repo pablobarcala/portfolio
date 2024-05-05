e3d4092b04a09f8505222cfaa60123799dff490f
pablobarcala <pablobarcala4@gmail.com>
2024-05-04 21:17:49 -0300

    crear mock de educaciones

---
+++ src/app/interfaces/mock-education.ts
@@ -0,0 +1,12 @@
+import { Education } from "./Education";
+
+export const EDUCATION: Education[] = [
+    {
+        id: 0,
+        name: "Argentina Programa 4.0",
+        title: "Desarrollador web Full Stack",
+        description: "Curso de desarrollo web Full Stack con tecnologías como Angular, Spring, Docker, MySQL, Figma y demás.",
+        img: "",
+        url: "https://drive.google.com/file/d/1bpRbE8yejbshPw8f-SKk5G18Tjq19cyB/view?usp=sharing"
+    }
+]
