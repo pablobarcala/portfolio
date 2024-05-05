3e64740b5e961afec62aead5ee64b787360f04de
pablobarcala <pablobarcala4@gmail.com>
2024-05-04 21:18:40 -0300

    crear componentes para las educaciones

---
+++ src/app/components/education-item/education-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-education-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './education-item.component.html',
+  styleUrl: './education-item.component.css'
+})
+export class EducationItemComponent {
+
+}
