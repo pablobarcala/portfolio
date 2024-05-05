f25f228079fe6a7530f3b1f825a01d4c99569708
pablobarcala <pablobarcala4@gmail.com>
2024-05-04 21:04:30 -0300

    arreglar descripcion personal en celular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { StackPageComponent } from './components/stack-page/stack-page.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: '/home', pathMatch: 'full'},
-    {path: 'home', component: HomeComponent}
+    {path: 'home', component: HomeComponent},
+    {path: 'stack', component: StackPageComponent}
 ];
