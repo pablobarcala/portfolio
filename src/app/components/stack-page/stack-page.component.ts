edebc78ef60a260e3175557de78c345f013d29c9
pablobarcala <pablobarcala4@gmail.com>
2024-05-09 12:07:48 -0300

    completar stack y pagina para mostrar stack completo

--- src/app/components/stack-page/stack-page.component.ts
+++ src/app/components/stack-page/stack-page.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { Stack } from '../../interfaces/Stack';
+import { STACK } from '../../interfaces/mock-stack';
+import { StackItemComponent } from '../stack-item/stack-item.component';
 
 @Component({
   selector: 'app-stack-page',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, StackItemComponent],
   templateUrl: './stack-page.component.html',
   styleUrl: './stack-page.component.css'
 })
 export class StackPageComponent {
-
+  stack: Stack[] = STACK
 }
--- src/app/interfaces/mock-stack.ts
+++ src/app/interfaces/mock-stack.ts
@@ -5,64 +5,126 @@ export const STACK: Stack[] = [
         id: 0,
         name: "Angular",
         img: "angular.svg",
-        type: "front"
+        type: ["front"]
     },
     {
         id: 1,
         name: "React",
         img: "react.svg",
-        type: "front"
+        type: ["front"]
     },
     {
         id: 2,
         name: "Spring",
         img: "spring.svg",
-        type: "back"
+        type: ["back"]
     },
     {
         id: 3,
         name: ".NET",
         img: "net.svg",
-        type: "back"
+        type: ["back"]
     },
     {
         id: 4,
         name: "MySQL",
         img: "mysql.svg",
-        type: "back"
+        type: ["back", "database"]
     },
     {
         id: 5,
         name: "Javascript",
         img: "js.svg",
-        type: "language"
+        type: ["language"]
     },
     {
         id: 6,
         name: "Typescript",
         img: "ts.svg",
-        type: "language"
+        type: ["language"]
     },
     {
         id: 7,
         name: "Java",
         img: "java.svg",
-        type: "language"
+        type: ["language"]
     },
     {
         id: 8,
         name: "C#",
         img: "c-sharp.svg",
-        type: "language"
+        type: ["language"]
     },
     {
         id: 9,
         name: "HTML",
-        img: "html.svg"
+        img: "html.svg",
+        type: ["front tools"]
     },
     {
         id: 10,
         name: "CSS",
-        img: "css.svg"
+        img: "css.svg",
+        type: ["front tools"]
+    },
+    {
+        id: 11,
+        name: "Postman",
+        img: "postman.svg",
+        type: ["testing"]
+    },
+    {
+        id: 12,
+        name: "Git",
+        img: "git.svg",
+        type: ["versioning"]
+    },
+    {
+        id: 13,
+        name: "Angular Material",
+        img: "material.svg",
+        type: ["front tools"]
+    },
+    {
+        id: 14,
+        name: "Firebase",
+        img: "firebase.svg",
+        type: ["cloud"]
+    },
+    {
+        id: 15,
+        name: "Docker",
+        img: "docker.svg",
+        type: ["cloud"]
+    },
+    {
+        id: 16,
+        name: "Bootstrap",
+        img: "bootstrap.svg",
+        type: ["front tools"]
+    },
+    {
+        id: 17,
+        name: "Jira",
+        img: "jira.svg",
+        type: ["others"]
+    },
+    {
+        id: 18,
+        name: "Notion",
+        img: "notion.svg",
+        type: ["others"]
+    },
+    {
+        id: 19,
+        name: "Figma",
+        img: "figma.svg",
+        type: ["others"]
+    },
+    {
+        id: 20,
+        name: "C",
+        img: "c.svg",
+        type: ["other language"]
     }
 ]
